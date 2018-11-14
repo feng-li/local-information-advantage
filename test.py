d70b76639eee36f2e72ed104c2c0724c2af472f1
Feng Li <m@feng.li>
2018-11-14 16:13:13 +0800

    Add a new scripy

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-d1 = {'id':[1,2,3,4],'emo':[0.1,0.3,0.4,0.7],"loc":['','','','']}
-d2 = {'id':[1,2,5,7],'loc': ['bj','sh','sc','gz']}
-
-f1 = pd.DataFrame(d1)
-f2 = pd.DataFrame(d2)
